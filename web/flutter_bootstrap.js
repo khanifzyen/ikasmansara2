b804a89a69c4e3d6921671f51b43b1c8a1bc41d6
khanifzyen <khanif.zyen@gmail.com>
2026-02-10 15:30:17 +0700

    update web

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2806081334"
+    serviceWorkerVersion: "2645235581"
   }
 });
